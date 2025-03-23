728f9637cf5bb0a16c70b9937b127b1bc304f542
Ben10-som <87426685+Ben10-som@users.noreply.github.com>
2025-03-23 09:18:26 +0000

    hello

--- import_koboo.R
+++ import_koboo.R
@@ -3,10 +3,10 @@ library(robotoolbox)
 
 # Configuration de l'API KoboToolbox
 kobo_setup(url = "https://kf.kobotoolbox.org",
-           token =Sys.getenv("my_token"))
+           token ="4d2d05b02a89fc97fe59d9b8a77337b47649ef59")
 
 # Récupération de l'asset spécifique
-asset <- kobo_asset(Sys.getenv("my_uid"))
+asset <- kobo_asset("a7XX5gzqV4VvVf9LLwiUuH")
 
 asset %>% kobo_data() %>% write_rds('df.rds')
 
