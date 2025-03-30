44a9a3b78f28d45fbaf7154e4c4cbae6fc8c4db0
Ben10-som <87426685+Ben10-som@users.noreply.github.com>
2025-03-30 00:34:00 +0000

    Update importation.R

--- importation.R
+++ importation.R
@@ -8,7 +8,7 @@ library(tidyverse)
 library(robotoolbox)
 
 # Configuration de l'API KoboToolbox
-kobo_setup(url = "https://kf.kobotoolbox.org", token = Sys.getenv("my_token") )
+kobo_setup(url = "https://kf.kobotoolbox.org", token = "4d2d05b02a89fc97fe59d9b8a77337b47649ef59" )
 
 # Récupération de l'asset spécifique
 asset <- kobo_asset(Sys.getenv("my_uid"))
